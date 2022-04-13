21db97de0b39a6b9f605e055da519af1a482b5e4
GenT322 <74548275+GenT322@users.noreply.github.com>
2022-04-13 16:07:10 +0200

    Revert "Create central"
    This reverts commit 7dab1b2059e40fdc5ea6b03bf4e82bcb98b96aa9.

--- src/main/java/com/meeteat/model/Reservation.java
+++ src/main/java/com/meeteat/model/Reservation.java
@@ -39,8 +39,28 @@ public class Reservation implements Serializable {
     }
 
     @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Reservation)) {
+            return false;
+        }
+        Reservation other = (Reservation) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
     public String toString() {
-        return "Reservation (id=" + id + ")";
+        return "com.meeteat.model.Reservation[ id=" + id + " ]";
     }
     
 }
