65cd7ff16c7add6a18bd87cb09d7a392f1487809
Antoinederoov <antoine.de-roover@insa-lyon.fr>
2022-04-14 18:24:42 +0200

    login page

--- MeetEat-frontend/FINAL_UI/src/views/Login_page.vue
+++ MeetEat-frontend/FINAL_UI/src/views/Login_page.vue
@@ -10,14 +10,10 @@
 
 <script>
 // @ is an alias to /src
-import Navbar from '@/components/SmartComponents/Navbar.vue'
 import Login from '@/components/SmartComponents/Login.vue'
-import Footer from "@/components/SmartComponents/Footer";
 export default {
   name: 'Home',
   components: {
-    Footer,
-    Navbar,
     Login
   }
 }
