008947cc19970622c825901412dc483f2fc1b020
Antoinederoov <antoine.de-roover@insa-lyon.fr>
2022-04-17 23:28:52 +0200

    view reviews

---
+++ MeetEat-frontend/FINAL_UI/src/components/SmartComponents/CardReview.vue
@@ -0,0 +1,77 @@
+<template>
+    <div class="card">
+        <div class="row d-flex">
+            <div class=""> <img class="profile-pic" src="https://i.imgur.com/V3ICjlm.jpg"> </div>
+            <div class="">
+                <h3 class="mt-2 mb-0">Mukesh Kumar</h3>
+                    <div class="row">
+                    <div class="col-2">
+                        <h4 class="text-note">4</h4>
+                    </div>
+                    <div class="col-10">
+                        <label class="star-active" for="1">★</label> 
+                        <label class="star-active" for="2">★</label> 
+                        <label class="star-active" for="3">★</label> 
+                        <label class="star-active" for="4">★</label>
+                        <label class="star-inactive" for="5">☆</label>
+                    </div>
+                    </div>
+            </div>
+            <div class="ml-auto">
+                <p class="text-muted pt-5 pt-sm-3">10 Sept</p>
+            </div>
+        </div>
+        <div class="row text-left">
+            <h4 class="blue-text mt-3">Excellent</h4>
+            <p class="content">Rosine is a superb girl ! She's very kind and she likes disovering new type of food.</p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "CardReview" //même nom que le nom de fichier
+}
+</script>
+
+<style scoped>
+
+.star-active {
+    color: #FBC02D;
+    font-size: 40px;
+    margin: 0;
+    padding: 0;
+}
+
+.star-inactive {
+    color: #CFD8DC;
+    margin: 0;
+    padding: 0;
+    font-size: 40px;
+}
+
+.blue-text {
+    color: #0091EA;
+    /*padding: 0;*/
+}
+
+.content {
+    font-size: 18px
+}
+
+.profile-pic {
+    width: 90px;
+    height: 90px;
+    border-radius: 100%;
+    margin-right: 30px
+}
+
+div.col-10, div.col-2{
+  padding: 0;
+}
+
+h4.text-note{
+  margin-top: 15px;
+}
+
+</style>
---
+++ MeetEat-frontend/FINAL_UI/src/components/SmartComponents/ViewReviews.vue
@@ -0,0 +1,178 @@
+<template>
+  <div class="container-fluid px-1 py-5 mx-auto">
+    <div class="row justify-content-center">
+        <div class="col-xl-7 col-lg-8 col-md-10 col-12 text-center mb-5">
+            <div class="card">
+              <h2 class="left">Rosine's reviews</h2>
+              <hr class="line-color">
+                <div class="row justify-content-left d-flex">
+                    <div class="col-md-4 d-flex flex-column">
+                        <div class="rating-box">
+                            <h1 class="pt-4">4.0</h1>
+                            <p class="">out of 5</p>
+                        </div>
+                        <div> 
+                          <span class="fa fa-star star-active mx-1"></span> 
+                        </div>
+                    </div>
+                    <div class="col-md-8">
+                        <div class="rating-bar0 justify-content-center">
+                            <table class="text-left mx-auto">
+                                <tr>
+                                    <td class="rating-label">Excellent</td>
+                                    <td class="rating-bar">
+                                        <div class="bar-container">
+                                            <div class="bar-5"></div>
+                                        </div>
+                                    </td>
+                                    <td class="text-right">15</td>
+                                </tr>
+                                <tr>
+                                    <td class="rating-label">Very Good</td>
+                                    <td class="rating-bar">
+                                        <div class="bar-container">
+                                            <div class="bar-4"></div>
+                                        </div>
+                                    </td>
+                                    <td class="text-right">23</td>
+                                </tr>
+                                <tr>
+                                    <td class="rating-label">Good</td>
+                                    <td class="rating-bar">
+                                        <div class="bar-container">
+                                            <div class="bar-3"></div>
+                                        </div>
+                                    </td>
+                                    <td class="text-right">10</td>
+                                </tr>
+                                <tr>
+                                    <td class="rating-label">Correct</td>
+                                    <td class="rating-bar">
+                                        <div class="bar-container">
+                                            <div class="bar-2"></div>
+                                        </div>
+                                    </td>
+                                    <td class="text-right">3</td>
+                                </tr>
+                                <tr>
+                                    <td class="rating-label">Incorrect</td>
+                                    <td class="rating-bar">
+                                        <div class="bar-container">
+                                            <div class="bar-1"></div>
+                                        </div>
+                                    </td>
+                                    <td class="text-right">0</td>
+                                </tr>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="">
+              <CardReview/>
+            </div>
+            <div class="">
+              <CardReview/>
+            </div>
+            <div class="">
+              <CardReview/>
+            </div>
+      </div>
+  </div>
+ </div>
+</template>
+
+<script>
+import CardReview from '@/components/SmartComponents/CardReview.vue'
+export default {
+  name: "ViewReviews",
+  components: {
+    CardReview,
+  }
+}
+</script>
+
+<style scoped>
+
+.left {
+    text-align: left;
+}
+
+.card {
+    border-radius: 5px;
+    background-color: #fff;
+    padding-left: 60px;
+    padding-right: 60px;
+    margin-top: 30px;
+    padding-top: 30px;
+    padding-bottom: 30px
+}
+
+.rating-box {
+    width: 130px;
+    height: 130px;
+    margin-right: auto;
+    margin-left: auto;
+    background-color: #FBC02D;
+    color: #fff;
+}
+
+.rating-label {
+    font-weight: bold
+}
+
+.rating-bar {
+    width: 300px;
+    padding: 8px;
+    border-radius: 5px;
+}
+
+.bar-container {
+    width: 100%;
+    background-color: #f1f1f1;
+    text-align: center;
+    color: white;
+    border-radius: 20px;
+    margin-bottom: 5px
+}
+
+.bar-5 {
+    width: 30%;
+    height: 13px;
+    background-color: #FBC02D;
+    border-radius: 20px
+}
+
+.bar-4 {
+    width: 50%;
+    height: 13px;
+    background-color: #FBC02D;
+    border-radius: 20px
+}
+
+.bar-3 {
+    width: 20%;
+    height: 13px;
+    background-color: #FBC02D;
+    border-radius: 20px
+}
+
+.bar-2 {
+    width: 10%;
+    height: 13px;
+    background-color: #FBC02D;
+    border-radius: 20px
+}
+
+.bar-1 {
+    width: 0%;
+    height: 13px;
+    background-color: #FBC02D;
+    border-radius: 20px
+}
+
+td {
+    padding-bottom: 10px
+}
+
+</style>
