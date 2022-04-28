db2b4f5d2120fdac299a0aa2e53e2335bfc27dba
Antoinederoov <antoine.de-roover@insa-lyon.fr>
2022-04-28 10:41:51 +0200

    modification account page

---
+++ MeetEat-frontend/FINAL_UI/src/views/ModificationAccountPage.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <NavbarReturnHome></NavbarReturnHome>
+    <br/>
+    <ModificationAccount></ModificationAccount>
+  </div>
+</template>
+
+<script>
+import NavbarReturnHome from "@/components/SmartComponents/NavbarReturnHome";
+import ModificationAccount from "@/components/SmartComponents/ModificationAccount"//;
+//import Footer from "@/components/SmartComponents/Footer"
+
+export default {
+  name: "ModificationAccountPage",
+  components: {NavbarReturnHome, ModificationAccount}
+}
+</script>
+
+<style scoped>
+
+</style>
