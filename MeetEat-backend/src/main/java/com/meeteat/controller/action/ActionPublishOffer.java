e1cffc5f41f75b9b8e1b840cc5f71331b872c6fd
GenT322 <74548275+GenT322@users.noreply.github.com>
2022-05-04 09:33:31 +0200

    Update ActionPublishOffer
    coherent to the service

--- MeetEat-backend/src/main/java/com/meeteat/controller/action/ActionPublishOffer.java
+++ MeetEat-backend/src/main/java/com/meeteat/controller/action/ActionPublishOffer.java
@@ -5,8 +5,13 @@
 package com.meeteat.controller.action;
 
 import com.meeteat.model.Offer.Offer;
-import com.meeteat.model.Offer.Reservation;
 import com.meeteat.service.Service;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.servlet.http.HttpServletRequest;
 
 /**
@@ -18,8 +23,15 @@ public class ActionPublishOffer extends Action{
     @Override
     public void executer(HttpServletRequest request){
         Service service = new Service();
+        DateFormat df = new SimpleDateFormat("dd-MM-yyyy");
         Long offerId = Long.parseLong(request.getParameter("offerId"));
-        Offer offer = service.publishOffer(offerId);
+        Date expirationDate = null;
+        try {
+            expirationDate = df.parse(request.getParameter("expirationDate"));
+        } catch (ParseException ex) {
+            Logger.getLogger(ActionPublishOffer.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        Offer offer = service.publishOffer(offerId, expirationDate);
         request.setAttribute("offer", offer);
     }
     
