e5ed11437bc21bf366f12c67dd2dd4f7ef9cef99
GenT322 <74548275+GenT322@users.noreply.github.com>
2022-05-05 11:03:24 +0200

    MakeRequest

--- MeetEat-frontend/FINAL_UI/src/components/SmartComponents/ResumeOrder.vue
+++ MeetEat-frontend/FINAL_UI/src/components/SmartComponents/ResumeOrder.vue
@@ -22,12 +22,11 @@
       <div class="col-sm">
         <div class="card border border-secondary" v-for="order in orders" :key="order.order.id" style="background-color: lightgray">
           <img class="card-img-top"
-               src="../../assets/lasagne.jpg" style="padding-left: 5px;"/>
+               v-bind:src="order.order.image" style="padding-left: 5px;"/>
           <div class="card-body" style="text-align: left;">
             <h5 class="card-title">{{order.order.title}}</h5>
             <p class="card-text">
-              <b>Delivery : (A changer)01/01/2022{{order.order.expirationDate}} </b><br/>
-              {{order.order.details}}<br/>
+              <b>Delivery : {{order.order.availableFrom}}-{{order.order.expirationDate}} </b>
             </p>
             <!--
             <p class="card-text" v-for="option in order.order.classifications" :key="option">
@@ -45,7 +44,7 @@
               </div>
               <div class="col-sm">
                 <p class="card-text">
-                  <b>Number of portions : {{order.qty}}</b>
+                  <b>Number of portions : {{order.qty}}/{{order.order.remainingPortions}}</b>
                 </p>
               </div>
             </div>
@@ -53,6 +52,7 @@
             <br/>
             <div class="input-group w-auto">
               <button class="btn btn-dark" type="button" @click="viewDetails(order.order.id)">View details</button>
+              <button class="btn btn-dark" type="button" @click="deleteItem(order)">Delete item</button>
             </div>
           </div>
         </div>
@@ -146,6 +146,12 @@ export default {
       router.replace('/orderPage/'+id)
     },
 
+    deleteItem : function (order)
+    {
+      this.orders.splice(order.id,1);
+      sessionStorage.setItem("basket", JSON.stringify(this.orders));
+    },
+
     pay : async function () {
       //createReservation
       console.log('pay');
@@ -157,10 +163,6 @@ export default {
       }
 
       router.replace('/orderPage');
-
-
-
-
 
     },
 
@@ -176,7 +178,7 @@ export default {
       for (let i = 0; i < arr.length; i++) {
         console.log(arr[i]);
         var element = null;
-        await axios.get(urlAPI + 'todo=consultOffer&offerId=' + arr[i].id)
+        await axios.get(urlAPI + 'todo=consultOffer&offerId=' + arr[i].order.id)
             .then(response => (element = response.data));
 
         this.orders.push({
